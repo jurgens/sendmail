4f61507f082116ef64c6afed2bcaef5ef83e66f1
Yura Omelchuk <jurgen@kodolabs.com>
2014-07-10 18:22:22 +0400

    rename mail controller to messages controller due to 'mail' reserved keyword

--- spec/features/mail_spec.rb
+++ spec/features/mail_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe 'Mail', type: :feature do
   specify 'compose new mail' do
     visit '/'
     click_on 'Compose'
-    fill_and_submit_mail_form to: 'jurgen@kodolabs.com', subject: 'New mail', body: "Hello, how are you doing?"
+
+    within '.page-header' do
+      expect(page).to have_content 'Compose'
+    end
+
+    fill_and_submit_mail_form to: 'jurgen@kodolabs.com', subject: 'New mail', body: 'Hello, how are you doing?'
     expect(page).to have_content 'success'
   end
 end
