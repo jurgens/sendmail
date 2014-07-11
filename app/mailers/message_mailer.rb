be650f7cf86c1fa9e59bbfb87ebeeb25c7543adb
Yura Omelchuk <jurgen@kodolabs.com>
2014-07-11 17:20:07 +0400

    include attachment into email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -7,6 +7,10 @@ class MessageMailer < ActionMailer::Base
     @subject  = message.subject
     @body     = message.body
 
+    if message.has_attachment?
+      attachments[message.attachment.identifier] = message.attachment
+    end
+
     mail(subject: @subject, to: @to)
   end
 end
